ce8e13b35b8e1d1e4d2b48014f7769a865903266
Skronkie <ebrockway01@gmail.com>
2022-03-22 20:22:36 -0500

    added post route for loging information and password verification

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { differenceBy } = require("lodash");
 const { User } = require("../../models");
 
 router.get("/", (req, res) => {
@@ -43,6 +44,23 @@ router.post("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: { email: req.body.email },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+    res.json({ user: dbUserData, message: "You are now logged in!" });
+  });
 });
 
 router.put("/:id", (req, res) => {
