ea3fe57c82d235de535ee3268755f7a3b5532114
Skronkie <ebrockway01@gmail.com>
2022-03-28 14:59:39 -0500

    added comment model to post and user routes.

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -27,10 +27,6 @@ router.get("/", (req, res) => {
           attributes: ["username"],
         },
       },
-      {
-        model: User,
-        attributes: ["username"],
-      },
     ],
   })
     .then((dbPostData) => res.json(dbPostData))
@@ -59,8 +55,12 @@ router.get("/:id", (req, res) => {
     ],
     include: [
       {
-        model: User,
-        attributes: ["username"],
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
       },
     ],
   })
